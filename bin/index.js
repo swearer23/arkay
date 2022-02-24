74149e3ae6c4e3df64b81d602874a626e4d18406
yangshiwei <yangshiwei@longfor.com>
2022-02-24 16:24:16 +0800

    create project ready

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,9 @@
 import { program } from 'commander'
 import * as workspace from '../lib/workspace.js'
 import * as material from '../lib/material.js'
+import inquirer from 'inquirer';
+import * as akatoshClient from '../lib/akatoshClient.js'
+import chalk from 'chalk'
 
 program
 .version('0.1.0')
@@ -12,7 +15,35 @@ program
   .command('init')
   .option('--force', 'forcefully override existing content in current working folder')
   .action( option => {
-    workspace.create(option.force)
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'git_username',
+        message: "setting your git username: \n git username: "
+      },
+      {
+        type: 'input',
+        name: 'akatosh_server',
+        message: "setting your akatosh server host (eg. http://akatosh.longfor.com): \n server url: "
+      }
+    ])
+    .then(async answers => {
+      const username = answers.git_username
+      const akatoshServer = answers.akatosh_server
+      try {
+        if (await akatoshClient.validateUsername(username, akatoshServer)) {
+          const extraOptions = {
+            username, akatoshServer
+          }
+          workspace.create(option.force, extraOptions)
+        } else {
+          console.log(chalk.red('ERROR: username is not valid'))
+        }
+      } catch (err) {
+        console.error(err)
+        console.log(chalk.red(err.message))
+      }
+    })
   })
 
 program
@@ -24,7 +55,12 @@ program
   })
 
 program
-  .command('remove')
+  .command('release')
+  .argument('<type>', 'material type to add [cp]')
+  .argument('[name]', 'component name')
+  .action((type, name) => {
+    material.release(type, name)
+  })
 
 program.parse()
 

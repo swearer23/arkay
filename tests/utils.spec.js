72465bcf879e30fe0d341156f198c4c5196c0116
yangshiwei <yangshiwei@longfor.com>
2022-03-01 13:32:36 +0800

    增加根据当前组件package.json文件生成新的版本号的功能

---
+++ tests/utils.spec.js
@@ -0,0 +1,10 @@
+import { generateTag } from '../lib/utils/index.js'
+
+describe('utils test suite', () => {
+  test('generateTag test case', () => {
+    process.chdir('./tests/__demo_workspace__')
+    expect(generateTag('awesome-button', 'patch')).toBe('0.0.16')
+    expect(generateTag('awesome-button', 'minor')).toBe('0.1.0')
+    expect(generateTag('awesome-button', 'major')).toBe('1.0.0')
+  })
+})
